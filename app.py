ff515f8dbaba2ef5b99861da1ec9f73673a4359d
Mau <mau@qmau.me>
2022-08-19 22:53:06 +0900

    add commands

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    with open('database', 'r') as f:
+    with open("database", "r") as f:
         count = f.readline()
-        if count == '':
+        if count == "":
             count = 0
         count = int(count) + 1
-        
-    with open('database', 'w') as f:
-        f.write(f'{count}')
+
+    with open("database", "w") as f:
+        f.write(f"{count}")
 
     return f"""<!DOCTYPE html>
         <html>
--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@ sudo apt install python3 idle3
 touch database
 pip install -r requirements.txt
 echo 0 >> database
-
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+flask --app app run
